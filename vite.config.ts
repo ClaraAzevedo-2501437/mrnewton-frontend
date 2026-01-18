eeaed5bc73fe0ae69a4d6e11ebc3cc523c8e5123
Clara Azevedo <2501437@estudante.uab.pt>
2026-01-18 01:24:07 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3000,
     proxy: {
       '/activity-api': {
@@ -19,5 +20,9 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/analytics-api/, '/api/v1/analytics')
       }
     }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000
   }
 });
